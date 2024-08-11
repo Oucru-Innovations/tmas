a7b5c8b9b3f0620e8460d984950049800587e1df
Vo Thanh Hoang Anh <alaine210902@gmail.com>
2024-08-11 22:38:56 +0700

    add analysis & extract mics

--- scripts/run_tmas.py
+++ scripts/run_tmas.py
@@ -6,18 +6,19 @@ from src.tmas.utils import load_image, load_plate_design
 
 def main():
     # Load image
-    image = load_image("data/sample_data/01-DR0013-DR0013-1-14.png")
+    image_path = "data/sample_data/01-DR0013-DR0013-1-14.png"
+    image = load_image(image_path)
     
     # Preprocess image
     # processed_image = preprocess_images(image)
     processed_image = image
     
     # Detect growth
-    detections = detect_growth(processed_image) # return {boxes, labels, scores, inference_time}
+    detections, inference_time = detect_growth(processed_image) # return detections growth_matrix and inference_time
     
     # Analyze and extract MIC
     plate_design = load_plate_design()
-    mic_values = analyze_and_extract_mic(detections, plate_design)
+    mic_values = analyze_and_extract_mic(image_path, image, detections, plate_design)
     
     # Output MIC values
     print(mic_values)
